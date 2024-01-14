eb5f89297a7e3921af9ffcf3102935161006c823
wuanshi <1497809786@qq.com>
2024-01-14 17:06:33 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,14 @@
+console.log('process.env', process.env.APP);
+
+
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
